715c423b8f2ee0699aba71c66d9164a37960b657
Agrima Prajapat <agrimaprajapat@gmail.com>
2026-06-28 22:55:12 +0530

    Weather Observation 12

---
+++ Sql/WeatherObservationStation12.sql
@@ -0,0 +1,7 @@
+--https://www.hackerrank.com/challenges/weather-observation-station-12/problem
+
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY NOT REGEXP '^[AEIOUaeiou]'
+AND CITY NOT REGEXP '[AEIOUaeiou]$';
