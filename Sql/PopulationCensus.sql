7b564eefe116387f4e7df68c7394e3d871d05eed
Agrima Prajapat <agrimaprajapat@gmail.com>
2026-07-18 22:18:30 +0530

    Population Census

---
+++ Sql/PopulationCensus.sql
@@ -0,0 +1,5 @@
+--https://www.hackerrank.com/challenges/asian-population/problem
+
+
+select sum(city.population) from city join country on city.countrycode=country.code where country.continent='asia';
+
