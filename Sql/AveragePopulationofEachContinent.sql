eaa6cb6bb6f0ad6eede54dd96ee69a5b45617614
Agrima Prajapat <agrimaprajapat@gmail.com>
2026-07-20 22:41:09 +0530

    Average Population of Each Continent

---
+++ Sql/AveragePopulationofEachContinent.sql
@@ -0,0 +1,8 @@
+--https://www.hackerrank.com/challenges/average-population-of-each-continent/problem
+
+SELECT COUNTRY.Continent,
+FLOOR(AVG(CITY.Population))
+FROM CITY
+JOIN COUNTRY
+ON CITY.CountryCode = COUNTRY.Code
+GROUP BY COUNTRY.Continent;
