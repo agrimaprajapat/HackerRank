cecc2ee33bedbebb0d07322781eff4092318bd8f
Agrima Prajapat <agrimaprajapat@gmail.com>
2026-07-24 23:40:21 +0530

    Challenges

---
+++ Sql/Challenges.sql
@@ -0,0 +1,35 @@
+--https://www.hackerrank.com/challenges/challenges/problem
+
+
+SELECT
+    h.hacker_id,
+    h.name,
+    COUNT(c.challenge_id) AS total_challenges
+FROM Hackers h
+JOIN Challenges c
+    ON h.hacker_id = c.hacker_id
+GROUP BY h.hacker_id, h.name
+HAVING
+    COUNT(c.challenge_id) = (
+        SELECT MAX(challenge_count)
+        FROM (
+            SELECT COUNT(*) AS challenge_count
+            FROM Challenges
+            GROUP BY hacker_id
+        ) t
+    )
+    OR
+    COUNT(c.challenge_id) IN (
+        SELECT challenge_count
+        FROM (
+            SELECT COUNT(*) AS challenge_count
+            FROM Challenges
+            GROUP BY hacker_id
+        ) t
+        GROUP BY challenge_count
+        HAVING COUNT(*) = 1
+    )
+ORDER BY
+    total_challenges DESC,
+    h.hacker_id ASC;
+
