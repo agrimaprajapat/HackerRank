22a2eff31fedf9b2396b326e09ae425811e65027
Agrima Prajapat <agrimaprajapat@gmail.com>
2026-07-27 23:17:41 +0530

    sql project planning

---
+++ Sql/sqlprojectplanning.sql
@@ -0,0 +1,25 @@
+--https://www.hackerrank.com/challenges/sql-projects/problem
+
+
+SELECT
+    s.Start_Date,
+    MIN(e.End_Date)
+FROM
+    (SELECT Start_Date
+     FROM Projects
+     WHERE Start_Date NOT IN (
+         SELECT End_Date
+         FROM Projects
+     )) s
+JOIN
+    (SELECT End_Date
+     FROM Projects
+     WHERE End_Date NOT IN (
+         SELECT Start_Date
+         FROM Projects
+     )) e
+ON e.End_Date > s.Start_Date
+GROUP BY s.Start_Date
+ORDER BY
+    DATEDIFF(MIN(e.End_Date), s.Start_Date),
+    s.Start_Date;
